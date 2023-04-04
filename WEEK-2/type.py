b906e780f35c86d9ee37b29a7624427d9cceac1b
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-04 10:37:33 +0100

    Introduction to Data Types

--- WEEK-2/type.py
+++ WEEK-2/type.py
@@ -3,3 +3,18 @@
 # If the object is a class, then the type of the class is returned.
 
 # Example 1: How type() works in Python?
+
+type(5)
+print(type(5))
+
+# Result in terminal >> <class 'int'>  
+# The int there is the Integer.
+
+
+# Boolean Data Type
+
+print(5 == 5)
+# Result in terminal >> True.
+
+
+
