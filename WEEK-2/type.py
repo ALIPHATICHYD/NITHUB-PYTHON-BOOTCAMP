2165ea17f0050580d92deb46cb32eabb8d441a5c
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-04 10:25:38 +0100

    first commit

---
+++ WEEK-2/type.py
@@ -0,0 +1,5 @@
+# The Type() Function
+# The type() function returns the type of the object passed to it.
+# If the object is a class, then the type of the class is returned.
+
+# Example 1: How type() works in Python?
