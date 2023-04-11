1634c23e3e77b1fc6a2d6fe466448a27875092ad
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-04-11 13:02:06 +0100

    Introduction to Python WEEK 1

--- WEEK-1/intro.py
+++ WEEK-1/intro.py
@@ -59,3 +59,15 @@ print('Age: ', age)
 print('Married: ', is_married)
 print('Skills: ', skills)
 print('Person information: ', person_info)
+
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'ALIPHATIC', 'HYDROCARBON', 'NIGERIA', 0, False
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
