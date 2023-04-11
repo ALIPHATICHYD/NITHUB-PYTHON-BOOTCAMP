2008f3b5de01fdfba583c776bb73d2a43300f9c2
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-04-11 13:04:08 +0100

    Integers in operators.py

---
+++ WEEK-2/operators.py
@@ -0,0 +1,13 @@
+# Arithmetic Operations in Python
+# Integers
+
+print('Addition: ', 1 + 2)
+print('Subtraction: ', 2 - 1)
+print('Multiplication: ', 2 * 3)
+print ('Division: ', 4 / 2)                         # Division in python gives floating number
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
+print('Division without the remainder: ', 7 // 2)   # gives without the floating number or without the remaining
+print('Modulus: ', 3 % 2)                           # Gives the remainder
+print ('Division without the remainder: ', 7 // 3)
+print('Exponential: ', 3 ** 2)                     # it means 3 * 3
