e165d7cf03e697749f8f41d5061d260a678e51fe
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-04-11 13:14:09 +0100

    Arithmetic Operations

--- WEEK-2/operators.py
+++ WEEK-2/operators.py
@@ -32,3 +32,23 @@ print('Multiplying complex number: ',(1 + 1j) * (1-1j))
 
 a = 3 # a is a variable name and 3 is an integer data type
 b = 2 # b is a variable name and 3 is an integer data type
+
+
+# Arithmetic operations and assigning the result to a variable
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+# I should have used sum instead of total but sum is a built-in function try to avoid overriding builtin functions
+print(total) # if you don't label your print with some string, you never know from where is  the result is coming
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
