3a539f4a232f29edabcc4ee67e7b07285bd5b925
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-04-11 12:54:34 +0100

    Adding Variables in Python

--- WEEK-1/intro.py
+++ WEEK-1/intro.py
@@ -27,3 +27,21 @@ if age > 30:
     print("He's older than me")
     if age < 30:
         print("But younger")
+        
+          
+
+# Variables in Python
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+    'firstname':'Asabeneh', 
+    'lastname':'Yetayeh', 
+    'country':'Finland',
+    'city':'Helsinki'
+    }
