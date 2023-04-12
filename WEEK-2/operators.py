b32ad5483595378c41b6a5eea4b1e7c2e9e4b023
ALIPHATIC <adegokefuad1@gmail.com>
2023-04-12 08:31:26 +0100

    Introduction to Operations

--- WEEK-2/operators.py
+++ WEEK-2/operators.py
@@ -114,8 +114,8 @@ print('True or False:', True or False)
 # Another way comparison 
 print('1 is 1', 1 is 1)                   # True - because the data values are the same
 print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
-print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
-print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
+print('A in ALIPHATIC', 'A' in 'ALIPHATIC') # True - A found in the string
+print('B in BORN', 'B' in 'BORN') # False -there is no uppercase B
 print('coding' in 'coding for all') # True - because coding for all has the word coding
 print('a in an:', 'a' in 'an')      # True
 print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
