3c4cfd3747f27c06dbc35e718fb59131681e7ae8
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-04-12 08:39:58 +0100

    Create list.py

---
+++ WEEK-2/list.py
@@ -0,0 +1,8 @@
+empty_list = list() # this is an empty list, no item in the list
+print(len(empty_list)) # 0
+
+fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
