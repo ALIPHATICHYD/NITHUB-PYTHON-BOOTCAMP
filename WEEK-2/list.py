7ddd7a76f24f8053ae63c3aaedfd09c8676dba0f
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-04-12 08:40:32 +0100

    Update list.py

--- WEEK-2/list.py
+++ WEEK-2/list.py
@@ -6,3 +6,14 @@ vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of ve
 animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
 web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
 countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+# Print the lists and it length
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal products:',animal_products)
+print('Number of animal products:', len(animal_products))
+print('Web technologies:', web_techs)
+print('Number of web technologies:', len(web_techs))
+print('Number of countries:', len(countries))
