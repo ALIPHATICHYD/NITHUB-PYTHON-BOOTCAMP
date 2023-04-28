ee8da4e72412c0bf9f1f3f8f6bff98e85c0ad0bf
Adegoke Fuad Ajibola <adegokefuad1@gmail.com>
2023-04-28 10:36:34 +0000

    Python Dictionaries

--- WEEK-4/dictionary.py
+++ WEEK-4/dictionary.py
@@ -10,6 +10,12 @@ print(students)
 # students.update({"total_number": 4})
 
 # print("\n\n")
-print(students.setdefault("age", 100))
+# print(students.setdefault("age", 100))
+
+
 
 print(students)
+
+my_set = set([3, 1, 8, 10, 10, 8])
+
+print(my_set)
