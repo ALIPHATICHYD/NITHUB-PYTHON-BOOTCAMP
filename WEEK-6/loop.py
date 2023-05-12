a4b3d71e2d03839ddd63ebda824252d91d3e3696
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-05-12 11:13:41 +0100

    Tasks on Loops

--- WEEK-6/loop.py
+++ WEEK-6/loop.py
@@ -8,3 +8,16 @@ while True:
     print(101 - counter)
     counter -= 1
     print()
+
+
+"""
+Task: Multiples of 7 between 1 and 1000
+"""
+
+counter = 1000
+
+while counter >= 1:
+    if counter % 7 == 0:
+        print(counter)
+
+    counter -= 1
