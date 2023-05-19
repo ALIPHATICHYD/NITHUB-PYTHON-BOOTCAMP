f3cf237d1162f4c788595beac1834762b106b983
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-05-19 10:52:15 +0100

    Rock Paper Scissors

---
+++ WEEK-7/first.py
@@ -0,0 +1,17 @@
+"""
+Write a program to simulate the famous rock-paper-scissors game
+
+
+Rules:
+Rock wins Scissors
+Scissors wins Paper
+Paper wins Rock
+
+Hint.
+1. There are two user: A human and a computer 
+2. The python random module to generate computer's play
+3. When user type in "q" or "quit", you should end the game.
+
+Bonus.
+Keep track of the score
+"""
