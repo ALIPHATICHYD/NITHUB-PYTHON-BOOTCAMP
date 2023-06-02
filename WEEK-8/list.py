eb3198f57cc96b82042ebe0a13051a6b3c47cdf0
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-02 18:55:24 +0100

    Generating a List of Uppercase

--- WEEK-8/list.py
+++ WEEK-8/list.py
@@ -6,3 +6,17 @@ print(squares)
 
 
 ## Filtering Even Numbers From A List
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evens = [x for x in numbers if x % 2 == 0]
+print(evens)
+# Output: [2, 4, 6, 8, 10]
+
+
+## Generating A List Of Uppercase 
+
+message = "Hello, World!"
+upper_letters = [char for char in message if char.isupper()]
+print(upper_letters)
+# Output: ['H', 'W']
+
