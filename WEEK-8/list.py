dc5a9050279ae16328b2ce07edb06ff72276594f
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-02 19:31:58 +0100

    List Comprehensions In Python

--- WEEK-8/list.py
+++ WEEK-8/list.py
@@ -21,4 +21,13 @@ print(upper_letters)
 # Output: ['H', 'W']
 
 
-## Combining Elements From Two Lists
+# Combining Elements From Two Lists
+
+numbers = [1, 2, 3]
+letters = ['A', 'B', 'C']
+combined = [(x, y) for x in numbers for y in letters]
+print(combined)
+# Output: [(1, 'A'), (1, 'B'), (1, 'C'), (2, 'A'), (2, 'B'), (2, 'C'), (3, 'A'), (3, 'B'), (3, 'C')]
+ 
+
+# List comprehensions are a concise and efficient way to create new lists, and they can often replace the need for explicit loops. However, it's important to use them judiciously to maintain code readability and understandability.
