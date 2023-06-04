e1aec35e2890e2353748d0bf404afbf8a3fb2e0f
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-04 12:21:47 +0100

    Rock Paper Scissors

--- WEEK-7/first.py
+++ WEEK-7/first.py
@@ -20,3 +20,15 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 score = {"human": 0, "computer": 0}
+
+def determine_winner(human_choice, computer_choice):
+    if human_choice == computer_choice:
+        return "tie"
+    elif (
+        (human_choice == "rock" and computer_choice == "scissors") or
+        (human_choice == "scissors" and computer_choice == "paper") or
+        (human_choice == "paper" and computer_choice == "rock")
+    ):
+        return "human"
+    else:
+        return "computer"  
