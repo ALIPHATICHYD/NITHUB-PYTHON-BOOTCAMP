3ce845e6d3ff1bd3cf8ea7c47c2e89c90a3f162b
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-26 14:58:09 +0100

    Conversions

---
+++ WEEK-9/last.py
@@ -0,0 +1,30 @@
+# Converting a string to an integer 
+
+def convert_to_int(string):
+    return int(string)
+
+print(convert_to_int("2"))
+
+
+# Multiplying 2 integers
+
+def multiply(num1, num2):
+    return num1 * num2
+
+print(multiply(2, 3))
+
+print(3/2)
+
+# Finding thr square root of a number
+
+def square_root(num):
+    return num ** 0.5
+
+print(square_root(4))
+
+# Finding the square of a number
+
+def square(num):
+    return num ** 2
+
+print(square(4))
