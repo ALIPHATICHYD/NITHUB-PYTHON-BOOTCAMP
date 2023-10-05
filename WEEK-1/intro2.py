d9c81de3ae725304c8555cec23e02cd60a2a5585
ALIPHATIC <adegokefuad1@gmail.com>
2023-10-05 23:10:50 +0100

    variables

--- WEEK-1/intro2.py
+++ WEEK-1/intro2.py
@@ -3,3 +3,12 @@ print("   / |")
 print("  /  |")
 print(" /   |")
 print("/____|")
+
+
+character_name = "ACE"
+character_age = "20"
+
+print("There was a man named " + character_name + ", ")
+print("he was " + character_age + " years old")
+print("he really liked the name " + character_name + ", ")
+print ("but didn't like being " + character_age + ".")
