91822b75b97c8a45407df7d52632bf6dea6f7658
ALIPHATIC <adegokefuad1@gmail.com>
2023-10-07 18:43:17 +0100

    Strings

--- WEEK-1/intro2.py
+++ WEEK-1/intro2.py
@@ -1,4 +1,4 @@
-# Variables
+# Variables  #Uncomment to run
 
 # print("    /|")
 # print("   / |")
@@ -18,10 +18,20 @@
 
 # Strings
 # Upper Cases and Lower Cases
-phrase = "Girrafe  Academy"
-print(phrase.upper())
-print(phrase.lower())
-print(phrase.isupper())
-print(phrase.islower())
-print(phrase.upper().isupper())
-print(phrase.lower().islower())
+# phrase = "Giraffe Academy"
+# print(phrase.upper())
+# print(phrase.lower())
+# print(phrase.isupper())
+# print(phrase.islower())
+# print(phrase.upper().isupper())
+# print(phrase.lower().islower())
+
+# # Dealing with Numbers in a String
+# print(len(phrase))
+# print(phrase[0])  # Strings get indexed starting from 0
+# print(phrase.index("A"))  # Returns the index of the first occurrence of the character
+
+# print (phrase.replace("Giraffe", "Elephant"))  # Replaces the first string with the second string in the variable
+
+
+# Working with Numbers
