837ce65fef842030c17cd30dd108320139ced80a
ALIPHATIC <adegokefuad1@gmail.com>
2023-10-07 17:55:12 +0100

    upper & lower cases

--- WEEK-1/intro2.py
+++ WEEK-1/intro2.py
@@ -1,14 +1,27 @@
-print("    /|")
-print("   / |")
-print("  /  |")
-print(" /   |")
-print("/____|")
+# Variables
+
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+# print(" /   |")
+# print("/____|")
 
 
-character_name = "ACE"
-character_age = "20"
+# character_name = "ACE"
+# character_age = "20"
 
-print("There was a man named " + character_name + ", ")
-print("he was " + character_age + " years old")
-print("he really liked the name " + character_name + ", ")
-print ("but didn't like being " + character_age + ".")
+# print("There was a man named " + character_name + ", ")
+# print("he was " + character_age + " years old")
+# print("he really liked the name " + character_name + ", ")
+# print ("but didn't like being " + character_age + ".")
+
+
+# Strings
+# Upper Cases and Lower Cases
+phrase = "Girrafe  Academy"
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.islower())
+print(phrase.upper().isupper())
+print(phrase.lower().islower())
