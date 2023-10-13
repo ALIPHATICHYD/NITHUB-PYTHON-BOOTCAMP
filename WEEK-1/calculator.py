9d640af30c9e1e89cdd8f7e914771bc9204d1e58
ALIPHATIC <adegokefuad1@gmail.com>
2023-10-13 20:28:19 +0100

    Mad Libs Game

---
+++ WEEK-1/calculator.py
@@ -0,0 +1,8 @@
+# Python Calculator
+
+num1 = input("Enter a number:")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2) # int are used for numbers only 
+result = float(num1) + float(num2) # float are used for decimal numbers or numbers only 
+
+print(result) 
---
+++ WEEK-1/mad-libs-game.py
@@ -0,0 +1,9 @@
+# mad Libs Game
+
+color = input("Enter a Color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a Celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
