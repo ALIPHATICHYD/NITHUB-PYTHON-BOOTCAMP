05c0bb77764b975f1b668cfdbdacc5bef7eb1ae7
ALIPHATIC <adegokefuad1@gmail.com>
2023-10-19 22:08:55 +0100

    Calculator 2.0

---
+++ WEEK-2/calculator2.0.py
@@ -0,0 +1,24 @@
+# Building a Better Calculator 
+
+# First thing to do is to get input from the user. Three variables will be created. One for the First Number, One for the Second Number and One for the Operator. So we basically store the values that the user inputs into those variables.
+
+num1 = float(input("Enter first number: ")) # Enter the value for the First Number
+
+op = input("Enter Operator: ") # Enter Operator
+
+num2 = float(input("Enter your second number: ")) # Enter the value for the Second Number 
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid Operator")
+
+ 
+
+
--- WEEK-2/functions.py
+++ WEEK-2/functions.py
@@ -3,11 +3,11 @@
 
 # Creating a Function
 
-def say_wagwan(name, age):
-    print("Wagwan " + name + ", you are "  + age + " innit?")
+# def say_wagwan(name, age):
+#     print("Wagwan " + name + ", you are "  + age + " innit?")
 
-say_wagwan("ALPHA", "21")
-say_wagwan("BETA", "17")
+# say_wagwan("ALPHA", "21")
+# say_wagwan("BETA", "17")
 
 # You can really pass any type of data 
 
@@ -16,12 +16,12 @@ say_wagwan("BETA", "17")
 # Return Statement
 # The Return Keyword can basically allow Python to return information from a function.
 
-def cube(num):
-    return num*num*num # This will return the value of num*num*num
+# def cube(num):
+#     return num*num*num # This will return the value of num*num*num
 
 
-result = cube(3)
-print(result)
+# result = cube(3)
+# print(result)
 
 
 
@@ -52,14 +52,24 @@ print(result)
 
 # Creating a Boolean Variable
 
-is_male = True
-is_tall = True
+# is_male = True
+# is_tall = False
 
-if is_male or is_tall:
-    print("You are a male or tall or both")
-else:
-    print("You are neither male nor tall")
+# if is_male or is_tall: # This is using the "or" operator
+#     print("You are a male or tall or both")
+# else:
+#     print("You are neither male nor tall")
 
+# If we were to use the "and" both conditions have to be true
+# We can also use another keyword called ELIF, and it stands for else if 
+# if is_male and is_tall:
+#     print("You are a tall male")
+# elif is_male and not(is_tall):
+#     print("You are a short male")
+# elif not(is_male) and is_tall:
+#     print("You are not a male but are tall")
+# else:
+#     print("You are not a male and not tall")
 
 
 # If Statements & Comparisons
@@ -67,7 +77,7 @@ else:
 # Logical Operators: and, or, not   (These are used to combine conditional statements)
 
 def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
+    if num1 >= num2 and num1 >= num3: # Using if, elif and else statements to create a comparison function.
         return num1 
     elif num2 >= num1 and num2 >= num3:
         return num2
