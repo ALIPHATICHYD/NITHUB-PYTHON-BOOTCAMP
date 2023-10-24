f45f18d672829ddbab0dcccf81c9f6cafebb069b
ALIPHATIC <adegokefuad1@gmail.com>
2023-10-24 20:54:54 +0100

    Python Translator

---
+++ WEEK-3/translator.py
@@ -0,0 +1,13 @@
+# Building a Translator
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+        # if letter.lower() in "aeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
