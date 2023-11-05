8dbd043323c6929a765a97f92ece61d517d1861e
ALIPHATIC <adegokefuad1@gmail.com>
2023-11-05 09:36:06 +0100

    if else loop

--- WEEK-3/translator.py
+++ WEEK-3/translator.py
@@ -3,9 +3,12 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-        # if letter.lower() in "aeiou":
-            translation = translation + "g"
+        # if letter in "AEIOUaeiou":
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation + letter
     return translation
