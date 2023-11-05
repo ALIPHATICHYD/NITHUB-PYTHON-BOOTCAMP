d67e39b09c5a5001bc00fa9cdc21c02fdb3bbf58
ALIPHATIC <adegokefuad1@gmail.com>
2023-11-06 00:25:55 +0100

    Learn Python: Beginners Course

--- WEEK-4/errors.py
+++ WEEK-4/errors.py
@@ -1,1 +1,10 @@
 # Try Except: Catching Errors In Python
+
+try:
+    answer = 10/10
+    number = int(input("Enter a number: "))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("Invalid Input") 
