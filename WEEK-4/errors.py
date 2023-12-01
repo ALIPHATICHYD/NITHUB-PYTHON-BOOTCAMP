ed309c3cd50c78a9b2f4d4f90e9232ece24607f8
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-01 19:19:51 +0100

    Opening Files in Python

--- WEEK-4/errors.py
+++ WEEK-4/errors.py
@@ -1,17 +1,27 @@
 # Try Except: Catching Errors In Python
 
-try:
-    answer = 10/10
-    number = int(input("Enter a number: "))
-    print(number)
-except ZeroDivisionError as err:
-    print(err)
-except ValueError:
-    print("Invalid Input")   
+# try:
+#     answer = 10/10
+#     number = int(input("Enter a number: "))
+#     print(number)
+# except ZeroDivisionError as err:
+#     print(err)
+# except ValueError:
+#     print("Invalid Input")   
 
 
 
 
 # Opening Files in Python
 
-open(employees.txt, "r")
+## open("errors.txt", "w", "r", "a", "r+") # "w" means write, "r" means read,"a" means append, "r+" means read and write
+
+
+errors_file = open("errors.txt", "r")
+for errors in errors_file.readlines():
+    print(errors)  # Read all lines in the file 
+
+print(errors_file.readable()) # Check if file is readable
+print(errors_file.readline()) # Read the first line
+
+errors_file.close() # Close the file
