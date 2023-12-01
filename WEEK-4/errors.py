606c1f8429f8041f1e5ee4968511b16fdcbd9c9f
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-01 19:26:38 +0100

    Learn Python: Beginners Course

--- WEEK-4/errors.py
+++ WEEK-4/errors.py
@@ -1,3 +1,5 @@
+# Uncomment the code below to see the output.
+
 # Try Except: Catching Errors In Python
 
 # try:
@@ -17,11 +19,11 @@
 ## open("errors.txt", "w", "r", "a", "r+") # "w" means write, "r" means read,"a" means append, "r+" means read and write
 
 
-errors_file = open("errors.txt", "r")
-for errors in errors_file.readlines():
-    print(errors)  # Read all lines in the file 
+# errors_file = open("errors.txt", "r")
+# for errors in errors_file.readlines():
+#     print(errors)  # Read all lines in the file 
 
-print(errors_file.readable()) # Check if file is readable
-print(errors_file.readline()) # Read the first line
+# print(errors_file.readable()) # Check if file is readable
+# print(errors_file.readline()) # Read the first line
 
-errors_file.close() # Close the file
+# errors_file.close() # Close the file
