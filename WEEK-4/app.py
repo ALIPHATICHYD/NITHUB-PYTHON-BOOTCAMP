17008ddb82ae41025b56841672ce75ecf340a25f
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-04 18:05:38 +0100

    Learning Python

---
+++ WEEK-4/app.py
@@ -0,0 +1,18 @@
+# Uncomment code and run app.py to see the output
+
+# Writing to Files in Python
+
+# employee_file = open("employees1.txt", "w")
+
+# employee_file.write("Toby - Human Resources")
+# employee_file.write("\nkelly - Customer Service")
+
+# employee_file.close()
+
+
+
+employee_file = open ("index.html", "w")
+
+employee_file.write("<p> This is HTML </p>")
+
+employee_file.close()
