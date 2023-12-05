26d6412724d195b3a56f10cad8a48cfada936320
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-05 09:14:19 +0100

    Learning Python

---
+++ WEEK-5/app.py
@@ -0,0 +1,7 @@
+## Modules & PIP in Python
+
+## A module is a python file containing a set of functions you want to include in an application that you are working on.
+
+import useful_tools
+
+print(useful_tools.roll_dice(10))
