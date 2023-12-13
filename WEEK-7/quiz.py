5c822401bd4e7bdb1536248d3eae642003c200b8
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-14 00:32:50 +0100

    Learning Python Quizes

---
+++ WEEK-7/quiz.py
@@ -0,0 +1,23 @@
+from question import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n",
+
+]
+
+
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b"),
+]
+
+def run_test(questions): 
+    score = 0
+    for question in questions: 
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
