21de1842ac84baeaae60bcb425aed68db133dbb7
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-14 11:38:17 +0100

    Learning Python: Beginers Course

--- WEEK-6/student.py
+++ WEEK-6/student.py
@@ -9,4 +9,10 @@ class Student:
         self.name = name
         self.major = major
         self.gpa = gpa 
-        self.is_on_probation = is_on_probation
+#        self.is_on_probation = is_on_probation
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
