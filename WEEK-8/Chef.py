9ce7a8b120477a0db9a8c4ff62bb936dff9023ba
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-14 13:46:35 +0100

    Learn Python: Beginners Course

---
+++ WEEK-8/Chef.py
@@ -0,0 +1,13 @@
+# Inheritance in Python
+
+class Chef:
+         
+        def make_chicken(self):
+            print("The chef makes a chicken")
+    
+        def make_salad(self):
+            print("The chef makes a salad")
+    
+        def make_special_dish(self):
+            print("The chef makes a bbq ribs")
+            
---
+++ WEEK-8/app.py
@@ -0,0 +1,4 @@
+from Chef import Chef
+
+myChef = Chef()
+myChef.make_chicken()
