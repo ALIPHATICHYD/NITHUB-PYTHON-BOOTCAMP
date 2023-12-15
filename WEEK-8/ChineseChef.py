b0e643e819229f4877d046cec2596986e505569f
ALIPHATIC <adegokefuad1@gmail.com>
2023-12-15 15:24:40 +0100

    Learn Python: Beiginners Course

---
+++ WEEK-8/ChineseChef.py
@@ -0,0 +1,14 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+    def make_chicken(self):
+        print("The chef makes a chicken")
+
+    # def make_salad(self):
+    #     print("The chef makes a salad")
+
+    # def make_special_dish(self):
+    #     print("The chef makes bbq ribs")
+    
+    # def make_fried_rice(self):
+    #     print("The chef makes fried rice")
--- WEEK-8/app.py
+++ WEEK-8/app.py
@@ -1,4 +1,10 @@
 from Chef import Chef
+from ChineseChef import ChineseChef
 
 myChef = Chef()
-myChef.make_chicken()
+# myChef.make_chicken()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+# myChineseChef.make_fried_rice()
+myChineseChef.make_special_dish()
